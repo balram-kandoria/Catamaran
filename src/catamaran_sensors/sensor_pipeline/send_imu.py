f46ceafa204f641c0fabdb08cebba5c0535cc68d
Balram <balramkandoria@gmail.com>
2024-02-20 21:19:24 -0800

    Adding sensor fusion for pitch and roll

--- src/catamaran_sensors/sensor_pipeline/send_imu.py
+++ src/catamaran_sensors/sensor_pipeline/send_imu.py
@@ -5,17 +5,31 @@ from rclpy.node import Node
 from rclpy import qos
 from sensor_msgs.msg import Imu
 from example_interfaces.msg import Float64
+from geometry_msgs.msg import Vector3
+import math
 
 class imuPublisher(Node):
     def __init__(self):
         super().__init__("imu_publisher")
-        self.imu_data_pub = self.create_publisher(Imu, "/imu/spherical_buoy/raw",10)
+        self.imu_data_pub = self.create_publisher(Vector3, "/imu/spherical_buoy/Quaternion",10)
         self.imu_data_sub = self.create_subscription(Imu, "/imu/spherical_buoy", self.imu_callback, 10)
         
         
-    def publish_data(self, newData: Imu):
-        newmsg = newData
-        self.get_logger().info(f"Publishing: {newmsg}")
+    def publish_data(self, Data: Imu):
+
+        newmsg = Data.linear_acceleration
+
+        acceleration_x = Data.linear_acceleration.x
+        acceleration_y = Data.linear_acceleration.y
+        acceleration_z = Data.linear_acceleration.z
+
+
+
+         # Calculate pitch and roll from accelerometer data
+        pitch_acc = math.atan2(-acceleration_x, math.sqrt(acceleration_y**2 + acceleration_z**2))
+        roll_acc = math.atan2(acceleration_y, math.sqrt(acceleration_x**2 + acceleration_z**2))
+
+        self.get_logger().info(f"Publishing: Pitch {pitch_acc} and Roll {roll_acc}")
         self.imu_data_pub.publish(newmsg)
 
     def imu_callback(self, data: Imu):
