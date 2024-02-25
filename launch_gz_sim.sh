6726e9bc0987d4cbf2a1b22ed3381043c056d44a
Balram <balramkandoria@gmail.com>
2024-02-24 22:59:04 -0800

    Complete catamaran hull iteration 1

--- launch_gz_sim.sh
+++ launch_gz_sim.sh
@@ -4,12 +4,12 @@ Start_IMU_PIPELINE_SBuoy="/bin/python3 /home/saturn/Desktop/Dev/catamaran/src/ca
 # Source catamaran
 source /home/saturn/Desktop/Dev/catamaran/install/setup.bash 
 
-# # Launch Gazebo
-# LIBGL_ALWAYS_SOFTWARE=1 gz sim waves.sdf &
+# Launch Gazebo
+LIBGL_ALWAYS_SOFTWARE=1 gz sim /home/saturn/Desktop/Dev/catamaran/maritime_simulation/worlds/waves.sdf &
 
 # Launch the Gazebo -> ROS bridge for sensor data
 ros2 launch catamaran_sensors subcriber.launch.py &
 
-gnome-terminal -- sh -c "bash -c \"$Start_IMU_PIPELINE_SBuoy; exec bash\"" &
+# gnome-terminal -- sh -c "bash -c \"$Start_IMU_PIPELINE_SBuoy; exec bash\"" &
 
 wait
